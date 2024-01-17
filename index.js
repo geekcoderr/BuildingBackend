7e1ad7853c7adaad1e5727a51b22e828f79d3e86
Nishant Maheshwari <mnishant222@gmail.com>
2024-01-18 03:30:45 +0530

    some more functionalities

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT;
+const mlData = require('./ml.json');
 
 app.get('/', (req, res) => {
-  res.send('<button><a href="/dora">click me </button>')
-})
+  res.send('<a href="/dora"><button>Click me</button></a>');
+});
 
-app.get('/dora',(req,res) => {
-    res.send('<p>Hello this is dora page</p>')
-})
+app.get('/dora', (req, res) => {
+  res.json(mlData);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
